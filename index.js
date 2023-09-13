844d31b768262bba2c3cde179b1cdfaf67fa1ecb
olgajoki <olga.jokinen@ramirent.fi>
2023-09-13 15:23:07 +0300

    3.7

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -105,6 +107,12 @@ app.post("/api/persons", (req, res) => {
     res.json(person);
   }
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
